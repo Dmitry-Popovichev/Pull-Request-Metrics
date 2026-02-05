cf36367ffd92705b3483e75596c58e98b74c04fd
Dmitry Popovichev <dmitry.popovichev@stfc.ac.uk>
2026-02-05 23:28:05 +0000

    FEAT: A simple Python script to retrieve the repos associated to the user.
    A main.py file with accompanying unit tests.
    Restructured the directory format.
    Added a pytest.ini file to direct tests to the tests/ dir.
    Ignored some directories created in VS Code.

--- src/main.py
+++ src/main.py
@@ -1,19 +1,39 @@
+#!/usr/bin/python3
+
+'''
+This script pulls GitHub Pull Request metrics from STFC Cloud repositories.
+#NOTE# At the moment it's a placeholder script with some basic code
+to test ansible playbooks.
+'''
+
 from github import Github
 from github import Auth
 import os
 
-token = os.getenv("GITHUB_TOKEN")
+def retrieve_repo_names(token:str) -> list[str]:
+    ''' 
+    A simple function that retrieves all GitHub repositiores assocaited with the user, 
+    using a personal GH token.
+    :param token: a string containing a github token
+    :return: A list of STFC Cloud repositories associated with the User token
+    :rtype: List
+    '''
+    auth = Auth.Token(token)
+    
+    g = Github(auth=auth)
+    repo_list = []
 
-if not token:
-    print("No token found")
+    for repo in g.get_user().get_repos():
+        repo_list.append(repo.name)
+        #print(f"The repo name is: {repo.name}\n")
 
-auth = Auth.Token(token)
+    g.close()
+    return repo_list
 
-g = Github(auth=auth)
+if __name__ == "__main__":
+    token = os.getenv("GITHUB_TOKEN")
+    if not token:
+        print("No token found in the environment")
 
-g = Github(base_url="https://github.com/stfc/cloud-image-builders/api/v3", auth=auth)
-
-for repo in g.get_user().get_repos():
-    print(repo.name)
-
-g.close()
+    retrieve_repo_names(token=token)
+    
