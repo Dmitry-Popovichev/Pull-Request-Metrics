fc9af6f596b36941133d8034d8e94061881a74ce
Dmitry Popovichev <dmitry.popovichev@stfc.ac.uk>
2026-03-25 02:46:36 +0000

    FEAT: Additional pr metric calculations, posting and tests
    This PR includes additional code that calculates the average time to merge and average time to first review within a repo.
    
    It also creates a gauge metric for Prometheus to scrape and posts the metrics much like the previous one.
    
    Unit tests have been added to cover the new code as well.

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,12 +2,15 @@ import pytest
 import sys
 from unittest.mock import patch, MagicMock
 
+from datetime import datetime
+import logging
 from src import vars as src_vars
 
 sys.modules.setdefault("vars", src_vars)
 
 from src.main import retrieve_list_of_prs
 from src.main import retrieve_all_merged_prs
+from src.main import average_time_to_merge
 
 
 @patch("src.main.Auth")
@@ -76,3 +79,74 @@ def test_retrieve_all_merged_function_returns_only_merged_prs():
     assert isinstance(result, list), "Should return a list"
     assert len(result) == 1, "Should have exactly one merged PR"
     assert result[0] == mock_pr1, "The merged PR should match the mock"
+
+
+def test_average_time_to_merge_returns_a_positive_float():
+
+    mock_repo_name = "The best repo ever"
+
+    mock_pr1 = MagicMock()
+    mock_pr1.created_at = datetime.strptime("2026-03-24 15:02:03+00:00", '%Y-%m-%d %H:%M:%S%z')
+    mock_pr1.merged_at = datetime.strptime("2026-03-24 15:08:30+00:00", '%Y-%m-%d %H:%M:%S%z')
+
+    list_of_prs = [mock_pr1]
+
+    result = average_time_to_merge(list_of_prs, mock_repo_name)
+
+    assert isinstance(result, float) and result > 0, "Should return a postive float"
+
+
+def test_pr_datetime_has_None_value_and_skips_and_logs_warning(caplog):
+    
+    mock_repo_name = "The best repo ever"
+
+    mock_pr1 = MagicMock()
+    mock_pr1.number = 1
+    mock_pr1.created_at = datetime.strptime("2026-03-24 15:02:03+00:00", '%Y-%m-%d %H:%M:%S%z')
+    mock_pr1.merged_at = None
+
+    list_of_prs = [mock_pr1]
+
+    with caplog.at_level(logging.WARNING):
+        result = average_time_to_merge(list_of_prs, mock_repo_name)
+
+    assert len(caplog.records) == 1, "There should be 1 record of a warning"
+    assert "Skipping pr #1 in The best repo ever due to missing datestamps (created_at=2026-03-24 15:02:03+00:00, merged_at=None)" in caplog.text
+    assert result == 0
+
+def test_if_difference_is_negative_then_function_should_log_an_error(caplog):
+
+    mock_repo_name = "The best repo ever"
+
+    mock_pr1 = MagicMock()
+    mock_pr1.number = 1
+    mock_pr1.created_at = datetime.strptime("2026-03-24 15:08:30+00:00", '%Y-%m-%d %H:%M:%S%z')
+    mock_pr1.merged_at = datetime.strptime("2026-03-24 15:02:03+00:00", '%Y-%m-%d %H:%M:%S%z')
+
+    list_of_prs = [mock_pr1]
+
+    with caplog.at_level(logging.ERROR):
+        result = average_time_to_merge(list_of_prs, mock_repo_name)
+
+    assert len(caplog.records) == 1, "There should be 1 record of an error"
+    assert "Skipping pr #1 as it has produced a negative difference, -387.0 seconds" in caplog.text
+    assert result == 0
+
+
+def test_when_datetime_has_missing_timezone_and_exception_is_thrown_with_error_logging(caplog):
+
+    mock_repo_name = "The best repo ever"
+
+    mock_pr1 = MagicMock()
+    mock_pr1.number = 1
+    mock_pr1.created_at = datetime.strptime("2026-03-24 15:02:03+00:00", '%Y-%m-%d %H:%M:%S%z')
+    mock_pr1.merged_at = datetime.strptime("2026-03-24 15:08:30", '%Y-%m-%d %H:%M:%S')
+
+    list_of_prs = [mock_pr1]
+
+    with caplog.at_level(logging.ERROR):
+        result = average_time_to_merge(list_of_prs, mock_repo_name)
+
+    assert len(caplog.records) == 1
+    assert "Something went wront when calculating difference" in caplog.text
+    assert result == 0
